34e3e3a6097677807b3a572494f9ad6225bfd967
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2024-04-05 18:21:57 +0530

    bug fix

--- app.js
+++ app.js
@@ -167,7 +167,6 @@ cron.schedule('59 23 * * *' , async () => {
     console.log(dab, dabp , expenses);
     let tmp = dab;
     console.log(dl)
-    dab =  dab + dabp + dl;
     dabp = tmp;
     expenses = 0;
 
